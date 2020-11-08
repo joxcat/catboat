6c7aa5ea9b92dc8c2c0e4cde891b2b7318342674
joxcat <blackksoulls@gmail.com>
2020-11-08 18:46:18 +0100

    better startup script

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-if [[ -f "catboat.log" ]]; then else
+if [[ -f "catboat.log" ]]; then; else
   echo "Cleaning up old log"
   mv catboat.log.old
 fi
-if [[ -d "./target/release/catboat" ]]; then else
+if [[ -d "./target/release/catboat" ]]; then; else
   echo "Building"
   cargo build --release
 fi
